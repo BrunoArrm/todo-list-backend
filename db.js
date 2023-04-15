bb44af9442334d166f218eae85bc345824819e20
Bruno Arruda <86083291+BrunoArrm@users.noreply.github.com>
2023-04-15 18:01:12 -0300

    deploy

--- db.js
+++ db.js
@@ -3,6 +3,8 @@ async function connect() {
     if ( global.connection && global.connection.state !== 'disconnected' )
         return global.connection;
 
+    console.log('aaaaaaaaaaaaaaaaaaaa', process.env.conn);
+
     const mysql = require('mysql2/promise');
     const connection = await mysql.createConnection(process.env.conn);
     console.log('Conectado ao banco!');
